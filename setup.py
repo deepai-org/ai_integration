3ba6844765af63690320dfc2510a8be924fb201c
Kevin Baragona <kmbaragona@gmail.com>
2019-04-05 04:16:08 -0700

    version is now 1.0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open("./README.md", 'r').read()
 # This call to setup() does all the work
 setup(
     name="ai_integration",
-    version="1.0.3",
+    version="1.0.4",
     description="AI Model Integration for python",
     long_description=README,
     long_description_content_type="text/markdown",
